f28001dc7415c0b5d5f17b8524a65a48522cee86
Grant Ellis <robert.grant.ellis@gmail.com>
2018-03-28 18:25:55 -0400

    add godoc comments to shared submodule

--- shared/helpers.go
+++ shared/helpers.go
@@ -6,6 +6,9 @@ import (
 	"github.com/pkg/errors"
 )
 
+// ParseIP attempts to parse the string representation of an IP address and
+// then returns it as net.IP. If there is an error, it will be returned
+// instead with an embedded stacktrace and friendly message.
 func ParseIP(ipString string) (net.IP, error) {
 
 	ip := net.ParseIP(ipString)
@@ -16,6 +19,8 @@ func ParseIP(ipString string) (net.IP, error) {
 	return ip, nil
 }
 
+// ValidatePort validates a port. If the port is invalid, the returned error
+// will have an embedded stacktrace and friendly message.
 func ValidatePort(port int) error {
 
 	if port < 1 || port > 65535 {
